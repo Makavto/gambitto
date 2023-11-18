11b34f926f06ae5a8539a30393ef6fbea4ca93ee
mmurzintsev <mmurzintsev@trotoscoping.online>
2023-11-18 14:43:18 +0300

    feature/frontend-init: +base scss variables +rename css to scss

--- gambitto-frontend/src/index.tsx
+++ gambitto-frontend/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/normalize.css';
-import './styles/fonts.css';
-import './styles/index.css';
+import './styles/variables.scss';
+import './styles/fonts.scss';
+import './styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
