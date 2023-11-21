b6439b9a881f2131cdc312e4db6eb595eb131fae
mmurzintsev <mmurzintsev@trotoscoping.online>
2023-11-22 00:23:34 +0300

    feature/profile-page: +user card +logout button

--- gambitto-frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ gambitto-frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import styles from './ProfilePage.module.scss';
+import Card from '../../components/Card/Card'
+import { useAppDispatch, useAppSelector } from '../../hooks/redux'
+import Button from '../../components/Button/Button';
+import { AuthAPI } from '../../services/AuthService';
+import { userSlice } from '../../store/reducers/userSlice';
+import { ButtonTypesEnum } from '../../utils/ButtonTypesEnum';
 
 function ProfilePage() {
+  const {user} = useAppSelector(state => state.userSlice);
+  const {setUser} = userSlice.actions;
+
+  const dispatch = useAppDispatch();
+
+  const [logoutUser] = AuthAPI.useLogoutUserMutation();
+
+  const onLogout = () => {
+    logoutUser().then(() => {
+      localStorage.removeItem('accessToken');
+      dispatch(setUser(null));
+    });
+  }
+
   return (
-    <div>ProfilePage</div>
+    <>
+      <div className={styles.pageWrapper}>
+          <Card>
+            {
+              user &&
+              <div className={styles.cardContainer}>
+                <div className={styles.cardItem}>
+                  <span className='textBig'>Имя пользователя: {user.username}</span>
+                  <div>
+                    <Button type={ButtonTypesEnum.Danger} onClick={onLogout}>Выйти</Button>
+                  </div>
+                </div>
+                <div className={styles.cardItem}>
+                  <span className="textSecondary">E-mail: {user.email}</span>
+                </div>
+                <div>
+                  <span className="textSecondary">Шахматист с {new Date(user.createdAt).toLocaleDateString()}</span>
+                </div>
+              </div>
+            }
+          </Card>
+      </div>
+    </>
   )
 }
 
