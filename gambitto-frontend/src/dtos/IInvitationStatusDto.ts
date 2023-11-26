f52412da92d9539bf3ba02ecde95fd9114c808ff
mmurzintsev <mmurzintsev@trotoscoping.online>
2023-11-26 15:36:16 +0300

    feature/frontend-websockets: +friendship ws frontend

---
+++ gambitto-frontend/src/dtos/IInvitationStatusDto.ts
@@ -0,0 +1,7 @@
+export interface IInvitationStatusDto {
+  id: number,
+  status: string,
+  statusFormatted: string,
+  createdAt: string,
+  updatedAt: string
+}
---
+++ gambitto-frontend/src/models/enums/FriendshipWsMethodsEnum.ts
@@ -0,0 +1,13 @@
+export enum FriendshipWsMethodsEnum {
+  Invite = 'invite',
+  DeclineInvitation = 'decline',
+  AcceptInvitation = 'accept',
+  Delete = 'delete'
+}
+
+export enum FriendshipWsServerMethodsEnum {
+  Invitation = 'invited',
+  Accepted = 'accepted',
+  Declined = 'declined',
+  Deleted = 'deleted',
+}
