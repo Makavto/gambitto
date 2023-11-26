c4334075e76afdde0f1ada049b7aeefa1bfbaf94
mmurzintsev <mmurzintsev@trotoscoping.online>
2023-11-26 14:31:40 +0300

    feature/frintend-websockets: +ws connection on login & register

--- gambitto-frontend/src/services/FriendshipService.ts
+++ gambitto-frontend/src/services/FriendshipService.ts
@@ -2,18 +2,15 @@ import { createApi } from "@reduxjs/toolkit/query/react"
 import { baseQueryWithReauth } from "../utils/baseQuery"
 import { createWsConnection } from "../utils/createWsConnection";
 
-const friendshipWs = createWsConnection('ws://localhost:5000/api/friendship');
+let friendshipWs = createWsConnection('ws://localhost:5000/api/friendship');
 
 export const FriendshipAPI = createApi({
   reducerPath: 'FriendshipAPI',
   baseQuery: baseQueryWithReauth,
   endpoints: (builder) => ({
-    friendshipWebsocket: builder.query<any, {method: string} | void>({
-      queryFn: async (arg) => {
-        if (arg?.method) {
-          friendshipWs.send(JSON.stringify({method: arg.method}))
-        }
-        return {method: '', data: {}}
+    createWs: builder.query<any, void>({
+      queryFn: async () => {
+        return {data: null}
       },
       async onCacheEntryAdded(
         arg,
@@ -21,18 +18,10 @@ export const FriendshipAPI = createApi({
       ) {
         try {
           await api.cacheDataLoaded
-          const listener = (event: MessageEvent) => {
-            const data = JSON.parse(event.data);
-            api.updateCachedData((draft) => {
-              return data
-            })
-          }
-          friendshipWs.addEventListener('message', listener)
+          friendshipWs = createWsConnection('ws://localhost:5000/api/friendship');
         } catch (error) {
           console.log(error)
         }
-        await api.cacheEntryRemoved
-        friendshipWs.close()
       }
     }),
 
