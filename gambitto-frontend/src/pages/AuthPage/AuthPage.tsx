dbb1182abc9e2e43c014472bd74cc3c7c2e0d71e
mmurzintsev <mmurzintsev@trotoscoping.online>
2023-11-26 14:31:40 +0300

    feature/frintend-websockets: +ws connection on login & register

--- gambitto-frontend/src/pages/AuthPage/AuthPage.tsx
+++ gambitto-frontend/src/pages/AuthPage/AuthPage.tsx
@@ -9,6 +9,8 @@ import Button from '../../components/Button/Button';
 import { isFetchBaseQueryErrorType } from '../../utils/fetchBaseQueryErrorCheck';
 import { ButtonTypesEnum } from '../../utils/ButtonTypesEnum';
 import Card from '../../components/Card/Card';
+import { ChessAPI } from '../../services/ChessService';
+import { FriendshipAPI } from '../../services/FriendshipService';
 
 interface IAuthForm {
   email: string;
@@ -24,6 +26,9 @@ function AuthPage() {
   const [loginUser, {data: userData, error: userError}] = AuthAPI.useLoginUserMutation();
 
   const [registerUser, {data: registerData, error: registerError}] = AuthAPI.useRegisterUserMutation();
+
+  const [createChessWs] = ChessAPI.useLazyCreateWsQuery();
+  const [createFriendshipWs] = FriendshipAPI.useLazyCreateWsQuery();
 
   const {setUser} = userSlice.actions;
 
@@ -56,10 +61,14 @@ function AuthPage() {
     if (!!userData) {
       dispatch(setUser(userData.user));
       localStorage.setItem('accessToken', userData.accessToken);
+      createChessWs();
+      createFriendshipWs();
     }
     if (!!registerData) {
       dispatch(setUser(registerData.user));
       localStorage.setItem('accessToken', registerData.accessToken);
+      createChessWs();
+      createFriendshipWs();
     }
   }, [userData, registerData]);
 
