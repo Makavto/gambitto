2df826dc7b9344edadd1263afa18d2de09d438d3
Maxim <murrrzintsev@mail.ru>
2023-12-04 15:05:53 +0300

    feature/stats: +frontend minor refactoring

--- gambitto-frontend/src/pages/GamePage/GamePage.tsx
+++ gambitto-frontend/src/pages/GamePage/GamePage.tsx
@@ -133,17 +133,13 @@ function GamePage() {
         !!chessGame && chessGame.gameStatus === 'declined' && <div>Соперник не захотел играть :(</div>
       }
       {
-        !!chessGame && (chessGame.gameStatus === 'blackWin' ||
-                        chessGame.gameStatus === 'whiteWin' ||
-                        chessGame.gameStatus === 'stalemate' ||
-                        chessGame.gameStatus === 'draw' ||
-                        chessGame.gameStatus === 'inProgress') &&
+        !!chessGame && chessGame.gameStatus !== 'invitation' && chessGame.gameStatus !== 'declined' &&
           <div className={styles.gameBlock}>
             <div className={styles.boardWrapper}>
               <div className='textBig'>{user?.id === chessGame.blackPlayerId ? chessGame.whitePlayerName : chessGame.blackPlayerName}</div>
               <ChessBoard
                 startingFen={activeMove ? getPositionAfterMove(activeMove.positionBefore, activeMove.moveCode) : undefined}
-                isMovingBlocked={activeMove?.number !== chessGame.gameMoves[chessGame.gameMoves.length - 1].moveNumber}
+                isMovingBlocked={activeMove?.number !== chessGame.gameMoves[chessGame.gameMoves.length - 1].moveNumber || chessGame.gameStatus !== 'inProgress'}
                 makeMove={onMakeMove}
                 boardOrientation={chessGame.blackPlayerId === user?.id ? 'black' : 'white'}
               />
