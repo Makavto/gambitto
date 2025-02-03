d1a190943e622aae591a27eb10ad0ce3ff726c17
Maxim <93901579+Makavto@users.noreply.github.com>
2025-02-03 23:19:52 +0300

    feature/resign-frontend: +resign function (#28)

--- gambitto-frontend/src/pages/GamePage/GamePage.tsx
+++ gambitto-frontend/src/pages/GamePage/GamePage.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { ChessAPI } from '../../services/ChessService'
-import { useAppSelector } from '../../hooks/redux';
-import { useParams } from 'react-router';
-import { IGameFullInfoDto } from '../../dtos/IGameFullInfoDto';
-import { ChessWsServerMethodsEnum } from '../../models/enums/ChessWsMethodsEnum';
-import ChessBoard from '../../components/ChessBoard/ChessBoard';
-import styles from './GamePage.module.scss';
-import { Chess } from 'chess.js';
+import React, { useEffect, useState } from "react";
+import { ChessAPI } from "../../services/ChessService";
+import { useAppSelector } from "../../hooks/redux";
+import { useParams } from "react-router";
+import { IGameFullInfoDto } from "../../dtos/IGameFullInfoDto";
+import { ChessWsServerMethodsEnum } from "../../models/enums/ChessWsMethodsEnum";
+import ChessBoard from "../../components/ChessBoard/ChessBoard";
+import styles from "./GamePage.module.scss";
+import { Chess } from "chess.js";
+import Button from "../../components/Button/Button";
+import { ButtonTypesEnum } from "../../utils/ButtonTypesEnum";
 
 interface IHistoryMove {
   moveCode: string;
@@ -15,25 +17,27 @@ interface IHistoryMove {
 }
 
 function GamePage() {
+  const { gameId } = useParams();
 
-  const {gameId} = useParams();
-
-  const [getGameInfo, {data: gameInfoData}] = ChessAPI.useLazyGetGameInfoQuery();
-  const [getChessNotification, {data: chessListenerData}] = ChessAPI.useLazyChessNotificationsListenerQuery();
-  const [makeMove, {data: makeMoveData}] = ChessAPI.useLazyMakeMoveQuery();
+  const [getGameInfo, { data: gameInfoData }] =
+    ChessAPI.useLazyGetGameInfoQuery();
+  const [getChessNotification, { data: chessListenerData }] =
+    ChessAPI.useLazyChessNotificationsListenerQuery();
+  const [makeMove, { data: makeMoveData }] = ChessAPI.useLazyMakeMoveQuery();
+  const [resign, { data: resignData }] = ChessAPI.useLazyResignQuery();
 
   const [chessGame, setChessGame] = useState<IGameFullInfoDto>();
 
-  const {chessWsReady} = useAppSelector(state => state.wsSlice);
+  const { chessWsReady } = useAppSelector((state) => state.wsSlice);
 
-  const {user} = useAppSelector(state => state.userSlice);
+  const { user } = useAppSelector((state) => state.userSlice);
 
   const [history, setHistory] = useState<IHistoryMove[][]>([]);
   const [activeMove, setActiveMove] = useState<IHistoryMove>();
 
   useEffect(() => {
     if (!!chessWsReady && !!gameId) {
-      getGameInfo({gameId: Number(gameId)});
+      getGameInfo({ gameId: Number(gameId) });
       getChessNotification();
     }
   }, [chessWsReady]);
@@ -43,32 +47,36 @@ function GamePage() {
     const gameMoves = gameFullInfo.gameMoves;
     if (gameMoves.length === 0) return;
     for (let i = 0; i < gameMoves.length; i = i + 2) {
-      if (!!gameMoves[i+1]) {
+      if (!!gameMoves[i + 1]) {
         newHistoryArray.push([
           {
             moveCode: gameMoves[i].moveCode,
             positionBefore: gameMoves[i].positionBefore,
-            number: gameMoves[i].moveNumber
+            number: gameMoves[i].moveNumber,
           },
           {
-            moveCode: gameMoves[i+1].moveCode,
-            positionBefore: gameMoves[i+1].positionBefore,
-            number: gameMoves[i+1].moveNumber
-          }
+            moveCode: gameMoves[i + 1].moveCode,
+            positionBefore: gameMoves[i + 1].positionBefore,
+            number: gameMoves[i + 1].moveNumber,
+          },
         ]);
       } else {
         newHistoryArray.push([
           {
             moveCode: gameMoves[i].moveCode,
             positionBefore: gameMoves[i].positionBefore,
-            number: gameMoves[i].moveNumber
-          }
+            number: gameMoves[i].moveNumber,
+          },
         ]);
       }
     }
-    setActiveMove(newHistoryArray[newHistoryArray.length - 1][newHistoryArray[newHistoryArray.length - 1].length - 1]);
+    setActiveMove(
+      newHistoryArray[newHistoryArray.length - 1][
+        newHistoryArray[newHistoryArray.length - 1].length - 1
+      ]
+    );
     setHistory(newHistoryArray);
-  }
+  };
 
   useEffect(() => {
     if (!!gameInfoData) {
@@ -80,109 +88,173 @@ function GamePage() {
   useEffect(() => {
     if (!!makeMoveData) {
       setChessGame(makeMoveData.gameFullInfo);
-      resetHistory(makeMoveData.gameFullInfo)
+      resetHistory(makeMoveData.gameFullInfo);
     }
-  }, [makeMoveData])
+  }, [makeMoveData]);
+
+  useEffect(() => {
+    if (!!resignData) {
+      setChessGame((prev) => ({
+        ...prev!,
+        gameStatus: resignData.game.gameStatus,
+        gameStatusFormatted: resignData.game.gameStatusFormatted,
+      }));
+    }
+  }, [resignData]);
 
   useEffect(() => {
     if (!!chessListenerData) {
-      if (chessListenerData.method === ChessWsServerMethodsEnum.Accepted && chessListenerData.data.game.id === Number(gameId)) {
+      if (
+        chessListenerData.method === ChessWsServerMethodsEnum.Accepted &&
+        chessListenerData.data.game.id === Number(gameId)
+      ) {
         setChessGame((prev) => ({
           ...prev!,
           gameStatus: chessListenerData.data.game.gameStatus,
-          gameStatusFormatted: chessListenerData.data.game.gameStatusFormatted
-        }))
+          gameStatusFormatted: chessListenerData.data.game.gameStatusFormatted,
+        }));
       }
-      if (chessListenerData.method === ChessWsServerMethodsEnum.Declined && chessListenerData.data.game.id === Number(gameId)) {
+      if (
+        chessListenerData.method === ChessWsServerMethodsEnum.Declined &&
+        chessListenerData.data.game.id === Number(gameId)
+      ) {
         setChessGame((prev) => ({
           ...prev!,
           gameStatus: chessListenerData.data.game.gameStatus,
-          gameStatusFormatted: chessListenerData.data.game.gameStatusFormatted
-        }))
+          gameStatusFormatted: chessListenerData.data.game.gameStatusFormatted,
+        }));
       }
-      if (chessListenerData.method === ChessWsServerMethodsEnum.MadeMove && chessListenerData.data.gameFullInfo.id === Number(gameId)) {
+      if (
+        chessListenerData.method === ChessWsServerMethodsEnum.MadeMove &&
+        chessListenerData.data.gameFullInfo.id === Number(gameId)
+      ) {
         setChessGame(chessListenerData.data.gameFullInfo);
         resetHistory(chessListenerData.data.gameFullInfo);
+      }
+      if (
+        chessListenerData.method === ChessWsServerMethodsEnum.Resigned &&
+        chessListenerData.data.game.id === Number(gameId)
+      ) {
+        setChessGame((prev) => ({
+          ...prev!,
+          gameStatus: chessListenerData.data.game.gameStatus,
+          gameStatusFormatted: chessListenerData.data.game.gameStatusFormatted,
+        }));
       }
     }
   }, [chessListenerData]);
 
   const onMakeMove = (moveCode: string) => {
-    makeMove({gameId: Number(gameId), moveCode});
-  }
+    makeMove({ gameId: Number(gameId), moveCode });
+  };
+
+  const onResign = () => {
+    resign({ gameId: Number(gameId) });
+  };
 
   const onMakeMoveActive = (newActiveMove: IHistoryMove) => {
     setActiveMove(newActiveMove);
-  }
+  };
 
   const getPositionAfterMove = (positionBefore: string, moveCode: string) => {
     const game = new Chess(positionBefore);
     game.move(moveCode);
     return game.fen();
-  }
+  };
 
   return (
     <div className={styles.pageWrapper}>
-      {
-        !chessGame && <div>Загрузка...</div>
-      }
-      {
-        !!chessGame && chessGame.gameStatus === 'invitation' && <div>Подождите, пока соперник войдёт в игру</div>
-      }
-      {
-        !!chessGame && chessGame.gameStatus === 'declined' && <div>Соперник не захотел играть :(</div>
-      }
-      {
-        !!chessGame && chessGame.gameStatus !== 'invitation' && chessGame.gameStatus !== 'declined' &&
+      {!chessGame && <div>Загрузка...</div>}
+      {!!chessGame && chessGame.gameStatus === "invitation" && (
+        <div>Подождите, пока соперник войдёт в игру</div>
+      )}
+      {!!chessGame && chessGame.gameStatus === "declined" && (
+        <div>Соперник не захотел играть :(</div>
+      )}
+      {!!chessGame &&
+        chessGame.gameStatus !== "invitation" &&
+        chessGame.gameStatus !== "declined" && (
           <div className={styles.gameBlock}>
             <div className={styles.boardWrapper}>
-              <div className='textBig'>{user?.id === chessGame.blackPlayerId ? chessGame.whitePlayerName : chessGame.blackPlayerName}</div>
+              <div className="textBig">
+                {user?.id === chessGame.blackPlayerId
+                  ? chessGame.whitePlayerName
+                  : chessGame.blackPlayerName}
+              </div>
               <ChessBoard
-                startingFen={activeMove ? getPositionAfterMove(activeMove.positionBefore, activeMove.moveCode) : undefined}
-                isMovingBlocked={activeMove?.number !== chessGame.gameMoves[chessGame.gameMoves.length - 1]?.moveNumber || chessGame.gameStatus !== 'inProgress'}
+                startingFen={
+                  activeMove
+                    ? getPositionAfterMove(
+                        activeMove.positionBefore,
+                        activeMove.moveCode
+                      )
+                    : undefined
+                }
+                isMovingBlocked={
+                  activeMove?.number !==
+                    chessGame.gameMoves[chessGame.gameMoves.length - 1]
+                      ?.moveNumber || chessGame.gameStatus !== "inProgress"
+                }
                 makeMove={onMakeMove}
-                boardOrientation={chessGame.blackPlayerId === user?.id ? 'black' : 'white'}
+                boardOrientation={
+                  chessGame.blackPlayerId === user?.id ? "black" : "white"
+                }
               />
               <div className={`textBig ${styles.user}`}>{user?.username}</div>
             </div>
             <div className={styles.statusWrapper}>
               <div>
-                Статус партии: <span className='textBig'>{chessGame.gameStatusFormatted}</span>
+                Статус партии:{" "}
+                <span className="textBig">{chessGame.gameStatusFormatted}</span>
               </div>
-              <div className={styles.historyWrapper}>
-                <div>История ходов:</div>
-                <div>
-                  {
-                    history.map((movePair, i) => (
+              {history.length > 0 && (
+                <div className={styles.historyWrapper}>
+                  <div>История ходов:</div>
+                  <div>
+                    {history.map((movePair, i) => (
                       <div className={styles.historyItem} key={i}>
                         {i + 1}.&nbsp;
                         <button
                           onClick={() => onMakeMoveActive(movePair[0])}
-                          className={`${styles.historyMove} ${movePair[0].number === activeMove?.number && styles.active}`}
+                          className={`${styles.historyMove} ${
+                            movePair[0].number === activeMove?.number &&
+                            styles.active
+                          }`}
                         >
                           {movePair[0].moveCode}
-                        </button>&nbsp;
-                        {
-                          !!movePair[1] &&
+                        </button>
+                        &nbsp;
+                        {!!movePair[1] && (
                           <>
-                          <button
-                            onClick={() => onMakeMoveActive(movePair[1])}
-                            className={`${styles.historyMove} ${movePair[1].number === activeMove?.number && styles.active}`}
-                          >
-                            {movePair[1].moveCode}
-                          </button>&nbsp;
+                            <button
+                              onClick={() => onMakeMoveActive(movePair[1])}
+                              className={`${styles.historyMove} ${
+                                movePair[1].number === activeMove?.number &&
+                                styles.active
+                              }`}
+                            >
+                              {movePair[1].moveCode}
+                            </button>
+                            &nbsp;
                           </>
-                        }
+                        )}
                       </div>
-                    ))
-                  }
+                    ))}
+                  </div>
                 </div>
-              </div>
+              )}
+              {chessGame.gameStatus === "inProgress" && (
+                <div className={styles.giveUpBtn}>
+                  <Button onClick={onResign} type={ButtonTypesEnum.Danger}>
+                    Сдаться
+                  </Button>
+                </div>
+              )}
             </div>
           </div>
-      }
+        )}
     </div>
-  )
+  );
 }
 
-export default GamePage
+export default GamePage;
