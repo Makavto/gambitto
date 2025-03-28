9b217404cee518d7b32722c03307d72bec6d1eb2
Maxim <93901579+Makavto@users.noreply.github.com>
2025-03-28 19:36:32 +0300

    fixes/backend-user-dto: +backend user dto update fix (#34)

--- gambitto-backend/controllers/userController.js
+++ gambitto-backend/controllers/userController.js
@@ -1,31 +1,37 @@
 const UserDto = require("../dtos/userDto");
 const ApiError = require("../error/ApiError");
 const userService = require("../service/userService");
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
-const {validationResult} = require('express-validator');
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+const { validationResult } = require("express-validator");
 
 class UserController {
-
   async register(req, res, next) {
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         const errorsArr = errors.array();
-        let errorMessage = errorsArr.length === 1 ? 'Поле заполнено некорректно: ' : 'Поля заполнены некорректно: ';
+        let errorMessage =
+          errorsArr.length === 1
+            ? "Поле заполнено некорректно: "
+            : "Поля заполнены некорректно: ";
         errorsArr.forEach((err, i) => {
-          err.path === 'email' && (errorMessage += 'e-mail');
-          err.path === 'password' && (errorMessage += 'пароль');
-          err.path === 'username' && (errorMessage += 'логин');
-          if (i !== errorsArr.length - 1) errorMessage += ', ';
-        })
+          err.path === "email" && (errorMessage += "e-mail");
+          err.path === "password" && (errorMessage += "пароль");
+          err.path === "username" && (errorMessage += "логин");
+          if (i !== errorsArr.length - 1) errorMessage += ", ";
+        });
         return next(ApiError.badRequest(errorMessage));
       }
-      const {email, password, username} = req.body;
+      const { email, password, username } = req.body;
       const userData = await userService.register(username, email, password);
-      res.cookie('refreshToken', userData.refreshToken, {maxAge: Number(process.env.JWT_REFRESH_EXPIRES_IN_MILLISECONDS), httpOnly: true, secure: false});
+      res.cookie("refreshToken", userData.refreshToken, {
+        maxAge: Number(process.env.JWT_REFRESH_EXPIRES_IN_MILLISECONDS),
+        httpOnly: true,
+        secure: false,
+      });
       return res.json(userData);
     } catch (error) {
-      return next(error)
+      return next(error);
     }
   }
 
@@ -34,31 +40,38 @@ class UserController {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         const errorsArr = errors.array();
-        let errorMessage = errorsArr.length === 1 ? 'Поле заполнено некорректно: ' : 'Поля заполнены некорректно: ';
+        let errorMessage =
+          errorsArr.length === 1
+            ? "Поле заполнено некорректно: "
+            : "Поля заполнены некорректно: ";
         errorsArr.forEach((err, i) => {
-          err.path === 'email' && (errorMessage += 'e-mail');
-          err.path === 'password' && (errorMessage += 'пароль');
-          if (i !== errorsArr.length - 1) errorMessage += ', ';
-        })
+          err.path === "email" && (errorMessage += "e-mail");
+          err.path === "password" && (errorMessage += "пароль");
+          if (i !== errorsArr.length - 1) errorMessage += ", ";
+        });
         return next(ApiError.badRequest(errorMessage));
       }
-      const {email, password} = req.body;
+      const { email, password } = req.body;
       const userData = await userService.login(email, password);
-      res.cookie('refreshToken', userData.refreshToken, {maxAge: Number(process.env.JWT_REFRESH_EXPIRES_IN_MILLISECONDS), httpOnly: true, secure: false});
+      res.cookie("refreshToken", userData.refreshToken, {
+        maxAge: Number(process.env.JWT_REFRESH_EXPIRES_IN_MILLISECONDS),
+        httpOnly: true,
+        secure: false,
+      });
       return res.json(userData);
     } catch (error) {
-      return next(error)
+      return next(error);
     }
   }
 
   async logout(req, res, next) {
     try {
-      const {refreshToken} = req.cookies;
+      const { refreshToken } = req.cookies;
       if (!refreshToken) {
-        next(ApiError.badRequest('User not authorized'))
+        next(ApiError.badRequest("User not authorized"));
       }
       const token = await userService.logout(refreshToken);
-      res.clearCookie('refreshToken');
+      res.clearCookie("refreshToken");
       return res.json(token);
     } catch (error) {
       return next(error);
@@ -67,9 +80,13 @@ class UserController {
 
   async refresh(req, res, next) {
     try {
-      const {refreshToken} = req.cookies;
+      const { refreshToken } = req.cookies;
       const userData = await userService.refresh(refreshToken);
-      res.cookie('refreshToken', userData.refreshToken, {maxAge: Number(process.env.JWT_REFRESH_EXPIRES_IN_MILLISECONDS), httpOnly: true, secure: true});
+      res.cookie("refreshToken", userData.refreshToken, {
+        maxAge: Number(process.env.JWT_REFRESH_EXPIRES_IN_MILLISECONDS),
+        httpOnly: true,
+        secure: true,
+      });
       return res.json(userData);
     } catch (error) {
       return next(error);
@@ -78,7 +95,7 @@ class UserController {
 
   async getUsers(req, res, next) {
     try {
-      const {searchQuery} = req.query;
+      const { searchQuery } = req.query;
       const users = await userService.getUsers(searchQuery, req.user.id);
       return res.json(users);
     } catch (error) {
@@ -88,7 +105,7 @@ class UserController {
 
   async getUserById(req, res, next) {
     try {
-      const {userId} = req.params;
+      const { userId } = req.params;
       const user = await userService.getUserById(userId);
       return res.json(user);
     } catch (error) {
@@ -98,32 +115,32 @@ class UserController {
 
   async getMe(req, res, next) {
     try {
-      const user = req.user;
-      return res.json(new UserDto(user))
+      const { id } = req.user;
+      const user = await userService.getUserById(id);
+      return res.json(new UserDto(user));
     } catch (error) {
-      return next(error)
+      return next(error);
     }
   }
 
-  async getTop(req,res,next) {
+  async getTop(req, res, next) {
     try {
       const usersTop = await userService.getTopUsers();
       return res.json(usersTop);
     } catch (error) {
-      return next(error)
+      return next(error);
     }
   }
 
-  async getUserStats(req,res,next) {
+  async getUserStats(req, res, next) {
     try {
-      const {userId} = req.query;
+      const { userId } = req.query;
       const stats = await userService.getUserStats(userId ?? req.user.id);
       return res.json(stats);
     } catch (error) {
       return next(error);
     }
   }
-
 }
 
 module.exports = new UserController();
